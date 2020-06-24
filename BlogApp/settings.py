135cace60ca6e06dc795cbd5b797168ad202e03a
Harish Somsole <66076559+Harish0512-design@users.noreply.github.com>
2020-06-25 00:59:08 +0530

    Static

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_HOST = 'smtp.gmail.com'

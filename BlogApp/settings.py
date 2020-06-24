9788ee9569d45e1e7edee20ece7b11df9e22a62a
Harish Somsole <66076559+Harish0512-design@users.noreply.github.com>
2020-06-25 01:47:02 +0530

    Debug

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG') == 'True')
+
 
 ALLOWED_HOSTS = ['*']
 
